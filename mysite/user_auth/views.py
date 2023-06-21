4d499d6f806f2f9e982f9ea7c8edb38ab5a547b7
QyDing02 <596719881@qq.com>
2023-06-22 01:34:09 +0800

    add LoginView

--- mysite/user_auth/views.py
+++ mysite/user_auth/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from django.contrib.auth.hashers import make_password
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -17,3 +18,13 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"detail": "Login Successful"}, status=status.HTTP_200_OK)
+        return Response({"detail": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
