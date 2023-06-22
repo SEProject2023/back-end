e229a36c87bf56b3bb6cf9e59fff08aa98843fea
QyDing02 <596719881@qq.com>
2023-06-23 01:23:53 +0800

    查看问题时能够返回对应的所有回答

--- mysite/questions/serializers.py
+++ mysite/questions/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Question
-from .models import User
+from answers.models import Answer
+from answers.serializers import AnswerSerializer
 
 class QuestionSerializer(serializers.ModelSerializer):
     asker = serializers.StringRelatedField(source='user.username')  # 将字段名从user_name改为asker
+    answers = AnswerSerializer(many=True, read_only=True)  # 一对多关系，所以要加上many=True
     class Meta:
         model = Question
         #下面就是api给前端的返回值
-        fields = ['asker', 'title', 'description']  # 从fields中移除'user_id'和'tags'
+        fields = ['asker', 'title', 'description', 'answers']  # 从fields中移除'user_id'和'tags'
