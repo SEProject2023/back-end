5ec47c120d5f17fec9c2a2f6daecd25879e6a607
QyDing02 <596719881@qq.com>
2023-06-23 01:00:24 +0800

    能运行，但是API返回值不符合预期

--- mysite/answers/serializers.py
+++ mysite/answers/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Answer
 from .models import User
+from questions.models import Question
 
 
 class AnswerSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(source='user', queryset=User.objects.all())
+    question_id = serializers.PrimaryKeyRelatedField(source='question', queryset=Question.objects.all())
     class Meta:
         model = Answer
         fields = ['user_id', 'question_id', 'content']
