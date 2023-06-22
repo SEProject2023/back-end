2f5fe1890bf38ea6bc93173f4d2c5a3ee7305af4
QyDing02 <596719881@qq.com>
2023-06-22 22:09:29 +0800

    return 5 values (question api)(it can run)

--- mysite/questions/serializers.py
+++ mysite/questions/serializers.py
@@ -4,7 +4,8 @@ from .models import User
 
 class QuestionSerializer(serializers.ModelSerializer):
     user_id = serializers.PrimaryKeyRelatedField(source='user', queryset=User.objects.all())
-
+    user_name = serializers.StringRelatedField(source='user.username')
     class Meta:
         model = Question
-        fields = ['user_id', 'title', 'description', 'tags']
+        #下面就是api给前端的返回值
+        fields = ['user_id', 'user_name', 'title', 'description', 'tags']  # 
