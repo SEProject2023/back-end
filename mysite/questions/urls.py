4a960e679812e6f4759161e63795eeff539904e8
QyDing02 <596719881@qq.com>
2023-06-22 21:42:03 +0800

    move "question" to sub-urls.py

--- mysite/questions/urls.py
+++ mysite/questions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import QuestionCreateView, QuestionDetailView
 
 urlpatterns = [
-    path('', QuestionCreateView.as_view(), name='create_question'),
-    path('/<int:pk>', QuestionDetailView.as_view(), name='detail_question'),
+    path('questions', QuestionCreateView.as_view(), name='create_question'),
+    path('questions/<int:pk>', QuestionDetailView.as_view(), name='detail_question'),
 ]
