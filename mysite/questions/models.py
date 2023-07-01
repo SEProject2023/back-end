63493017c9e76001e90dc8cfe5e40d0041a78ffa
QyDing02 <596719881@qq.com>
2023-07-01 15:57:36 +0800

    add time for questions model

--- mysite/questions/models.py
+++ mysite/questions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from datetime import datetime
 
 User = get_user_model()
 
@@ -11,6 +12,7 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     tags = models.CharField(max_length=200)
+    time = models.DateTimeField(auto_now_add=True, default=datetime.now)
 
     def __str__(self):
         return self.title
